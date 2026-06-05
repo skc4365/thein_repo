d6baa7e90a3abbe38064019f4772bc494ac8015e
skc4365 <50658047+skc4365@users.noreply.github.com>
2026-06-05 16:16:08 +0900

    add 에러

---
+++ proj_ex0605/src/com/skc4/proj_ex0605/MyMain4.java
@@ -0,0 +1,37 @@
+package com.skc4.proj_ex0605;
+
+public class MyMain4 {
+	
+	public static void main(String[] args) {
+		// 오류
+//		1. 세미콜론 누락
+//		int x = 5
+//		System.out.println(x);
+		
+//		2. 선언되지 않은 변수
+//		System.out.println(myVar);
+		
+//		3. 유형 불일치
+//		int x = "Hello";
+		
+		// 런타임 오류
+//		1) 0으로 나누기
+//		int x = 10;
+//		int y = 0;
+//		int result = x / y;
+//		System.out.println(result);
+		
+//		2) 배열 인덱스 범위를 벗어남
+//		int[] numbers = {1, 2, 3};
+//		System.out.println(numbers[8]);
+		
+		// 논리적 오류
+//		int x = 10;
+//		int y = 2;
+//		int sum = x - y;
+//		System.out.println("x + y = " + sum);
+		
+		
+	}
+
+}
