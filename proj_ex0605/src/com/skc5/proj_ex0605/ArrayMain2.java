2e1851acc61940b481a8d50b06bc05ebc205fc8c
skc4365 <50658047+skc4365@users.noreply.github.com>
2026-06-05 16:24:01 +0900

    add add ArrayList연습2

---
+++ proj_ex0605/src/com/skc5/proj_ex0605/ArrayMain2.java
@@ -0,0 +1,26 @@
+package com.skc5.proj_ex0605;
+
+import java.util.ArrayList;
+
+public class ArrayMain2 {
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> scores = new ArrayList<Integer>();
+		
+		scores.add(80);
+		scores.add(90);
+		scores.add(70);
+		scores.add(100);
+		System.out.println(scores);
+		
+		int sum = 0;
+		
+		for(int s:scores) {
+			sum += s;
+		}
+		double avg = (double)(sum / scores.size());
+		System.out.println("총점: " + sum);
+		System.out.println("평균: " + avg);
+	}
+
+}
